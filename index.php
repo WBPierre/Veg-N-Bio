30196132185548199745902a3893dce6da9bb4e0
Tristan Luong <tristan.luong@canaltp.fr>
2018-02-19 23:37:53 +0100

    New version folder, need advise

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php include './src/components/Header/index.php'; ?>
+
+<body>
+    <div style="height: 100vh">
+        <div class="flex-center flex-column">
+            <h1 class="animated fadeIn mb-4">Material Design for Bootstrap</h1>
+
+            <h5 class="animated fadeIn mb-3">Thank you for using our product. We're glad you're with us.</h5>
+
+            <p class="animated fadeIn text-muted">MDB Team</p>
+            <button type="button" class="btn btn-primary btn-rounded">Primary</button>
+
+        </div>
+    </div>
+</body>
+
+
+
+
+<link rel="stylesheet" href="./src/stylesheets/Home/style.css">
+
+<?php include './src/components/Footer/index.php'; ?>
