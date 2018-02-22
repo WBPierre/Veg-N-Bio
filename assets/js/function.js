5d5f09cc85a660fd353dfaa97db9096798f06b76
Tristan Luong <tristan.luong@canaltp.fr>
2018-02-22 12:11:56 +0100

    Add google map on the map

---
+++ assets/js/function.js
@@ -0,0 +1,72 @@
+new WOW().init();
+
+$(document).ready(function(){
+
+
+
+    // Init MDB library
+
+    $('.mdb-select').material_select();
+
+    // Init Fading MDB
+
+    $(function () {
+        var selectedClass = "";
+        $(".filter").click(function () {
+            selectedClass = $(this).attr("data-rel");
+            $("#gallery").fadeTo(100, 0.1);
+            $("#gallery div").not("." + selectedClass).fadeOut().removeClass('animation');
+            setTimeout(function () {
+                $("." + selectedClass).fadeIn().addClass('animation');
+                $("#gallery").fadeTo(300, 1);
+            }, 300);
+        });
+    });
+
+
+    // Init Google Map
+
+    var map;
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 48.849145, lng: 2.389659},
+        zoom: 15
+    });
+
+    var infoWindow = new google.maps.InfoWindow({map: map});
+
+    // Try HTML5 geolocation.
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map
+            });
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+            'Error: The Geolocation service failed.' :
+            'Error: Your browser doesn\'t support geolocation.');
+    }
+
+
+
+});
