0bd3d5f732e9695d17d60fbe77273734e31a1df3
Tristan Luong <tristan.luong@canaltp.fr>
2018-03-01 14:51:21 +0100

    Modify text and picture and add gridstack in the conf

--- assets/js/function.js
+++ assets/js/function.js
@@ -4,6 +4,18 @@ $(document).ready(function() {
 
 
 
+    // Init GridStack
+
+    $(function () {
+        $('.grid-stack')
+            .gridstack()
+            .on('dragstart', function(event, ui) {
+                var grid = this;
+                var element = event.target;
+                console.log('test')
+            });
+    });
+
     // Init MDB library
 
     $('.mdb-select').material_select();
@@ -69,18 +81,55 @@ $(document).ready(function() {
 
     var state = 'img';
 
+    $('.adminEdition').hover(function() {
+        $(this).css({
+            'transform': 'scale(1.1)',
+            'cursor': 'pointer'
+        });
+        $(this).attr({
+            'data-toggle': 'tooltip',
+            'title': 'Click to modify',
+            'data-placement': 'top'
+
+        });
+        $(this).tooltip('show')
+    },
+        function (){
+            $(this).css("transform","scale(1)");
+    });
+
+
+
     $('.adminEdition').click(function () {
 
-        console.log(this);
+        if (this.tagName === 'IMG') {
 
-        if(this.tagName === 'IMG') {
             state = 'img';
-            $("#editionPicture").prop('hidden', false).attr('src', this.src);
-            $("#editionText").prop('hidden', true);
+
+            var img = new Image();
+            img = this;
+
+            $("#editionPicture").attr({
+                'hidden': false,
+                'src': img.src
+            });
+            $("#editionText").attr('hidden', true);
+            $("#showText").attr('hidden', true);
+            $("#file-field").attr('hidden', false);
         } else {
-            state = 'text';
-            $("#editionText").text(this.innerHTML).prop('hidden', false);
-            $("#editionPicture").prop('hidden', true);
+
+            state = 'texte';
+
+            var txt = new Text();
+            txt = this;
+
+            console.log(txt);
+
+            $("#showText").text(txt.innerHTML).attr('hidden', false);
+            $("#editionText").text($("#showText").text()).attr('hidden', false);
+            $("#editionPicture").attr('hidden', true);
+            $("#file-field").attr('hidden', true);
+
         }
 
         $('#fullHeightModalRight').modal('show');
@@ -88,49 +137,40 @@ $(document).ready(function() {
         var splitFile;
         var file;
 
-        if(state = 'img'){
-            $('#validFile').click(function () {
+        if (state === 'img') {
 
+            $.ajax({
+                url: '../../src/ImageController.php', // Le nom du script a changé, c'est send_mail.php maintenant !
+                type: 'GET', // Le type de la requête HTTP, ici devenu POST
+                data: 'src=' + img.src,
+            });
+            $("#inputFile").change(function () {
                 splitFile = $("#inputFile").val().split('\\');
                 file = splitFile[2].split('.');
-
                 $("#editionPicture").attr('src', '../assets/images/' + splitFile[2]);
-
             });
+
             $('#saveValidFile').click(function () {
-                $('.adminEdition').attr('src', '../assets/images/' + splitFile[2]);
+                $(img).attr('src', '../assets/images/' + splitFile[2]);
                 $('#fullHeightModalRight').modal('hide');
-            });
-        };
 
+                img = new Image();
+            });
+        }
+        else {
+
+            $("#editionText").keyup(function () {
+                $("#showText").text($("#editionText").val());
+            });
+
+            $('#saveValidFile').click(function () {
+
+                $(txt).text($("#showText").html());
+                $('#fullHeightModalRight').modal('hide');
+
+                txt = new Text();
+            });
+        }
 
     });
 });
-    /*
-
-        $('#fullHeightModalRight').modal('show')
-        if ($('#validFile').click(function () {
-
-                var file = document.getElementById('inputFile').value
-                var nameFile = file.split("\\");
-                var splitFile = nameFile[2].split('.');
-
-                if ((file).length > 1) {
-                    console.log(splitFile[1].toLowerCase());
-                    if (splitFile[1].toLowerCase() === 'jpg') {
-                        console.log('Le fichier est bon');
-                        console.log(file);
-                        $("#imageEdition").attr('src', '../assets/images/' + nameFile[2]);
-                    }
-                } else {
-                    console.log("Le fichier n'est pas bon");
-                }
-            })) ;
-        if ($('#saveValidFile').click(function () {
-                $("#imageEdition").attr('src', '../assets/images/' + nameFile[2]);
-            })) ;
-
-    });
-
-    */
-
