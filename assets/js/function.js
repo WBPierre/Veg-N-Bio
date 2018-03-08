487fff34670bbaf031a4cefbb65326557899d25b
Tristan Luong <tristan.luong@canaltp.fr>
2018-03-08 09:58:16 +0100

    Add google map section

--- assets/js/function.js
+++ assets/js/function.js
@@ -2,20 +2,6 @@ new WOW().init();
 
 $(document).ready(function() {
 
-
-
-    // Init GridStack
-
-    $(function () {
-        $('.grid-stack')
-            .gridstack()
-            .on('dragstart', function(event, ui) {
-                var grid = this;
-                var element = event.target;
-                console.log('test')
-            });
-    });
-
     // Init MDB library
 
     $('.mdb-select').material_select();
@@ -42,12 +28,34 @@ $(document).ready(function() {
 
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 48.849145, lng: 2.389659},
-        zoom: 15
+        zoom: 12
     });
 
     var infoWindow = new google.maps.InfoWindow({map: map});
 
     // Try HTML5 geolocation.
+
+    var positionMarker = {
+        Nation: { lat: 48.84839399999999 , lng: 2.395909999999958},
+        Bastille: { lat: 48.8530842 , lng: 2.369251599999984},
+        Republique: { lat: 48.867503, lng: 2.3638109999999415},
+        PlaceItalie: { lat:  48.830816, lng: 2.3553901000000224}
+    };
+
+    var counter = 1;
+    var markers = [];
+
+    for(var property in positionMarker){
+
+        var marker = new google.maps.Marker({
+            position: positionMarker[property],
+            map: map,
+            names: {
+                name: positionMarker
+            }
+        });
+        markers.push(marker);
+    }
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -60,10 +68,6 @@ $(document).ready(function() {
             infoWindow.setContent('Location found.');
             map.setCenter(pos);
 
-            var marker = new google.maps.Marker({
-                position: pos,
-                map: map
-            });
         }, function () {
             handleLocationError(true, infoWindow, map.getCenter());
         });
@@ -78,6 +82,24 @@ $(document).ready(function() {
             'Error: The Geolocation service failed.' :
             'Error: Your browser doesn\'t support geolocation.');
     }
+
+    $(".list-group-item-action").click(function(){
+
+
+        var list = new Text();
+        list = this;
+
+        var splitNameList = list.id.split('-');
+
+        for( i = 0; i < 4; i++) {
+            console.log(markers[i].position.lat);
+
+        }
+    });
+
+
+    $('p').addClass('adminEdition');
+    $('img').addClass('adminEdition');
 
     var state = 'img';
 
