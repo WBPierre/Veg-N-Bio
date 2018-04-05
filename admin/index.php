133b32cc71ee5143dfef2a5c26aba99fe5810a24
WBPierre <d.pierrebox@gmail.com>
2018-04-05 12:30:05 +0200

    Check module added

--- admin/index.php
+++ admin/index.php
@@ -17,19 +17,38 @@ if(preg_match('/success/', $url)){
 	$success = true;
 	$url = preg_replace('/\?success/', "", $url);
 }
-if(isset($_SESSION['adminLog']) && !empty($_SESSION['adminLog']) && $_SESSION['adminLog'] == true){
+if(isset($_SESSION['adminLog']) && !empty($_SESSION['adminLog']) && $_SESSION['adminLog'] == true && $_SESSION['access_level'] >= 1){
+	$check['id'] = $_SESSION['id'];
 	$string = LanguageController::translate('dashboard');
+	$request = new DatabaseController();
+	$today = date("Y-m-d");
+	if(($getData = $request->fetchAll("SELECT vnb_contract_check.date_inserted FROM vnb_contract, vnb_contract_check WHERE vnb_contract.id_employee = :id AND vnb_contract_check.id_contract = vnb_contract.id AND vnb_contract_check.date_inserted LIKE '".$today."%'", $check)) == NULL){
+		$error = true;
+		$badge = false;
+		$time = "";
+	}else{
+		$badge = true;
+		$max = max(array_keys($getData));
+		$time = substr($getData[$max]['date_inserted'],11,16);
+	}
 	switch($url){
 		case 'employeesManagement':
 			$data = new EmployeeManagerController();
 			$array = $data->getAllEmployees();
-			echo $twig->render('employeesManagement/employeesManagement.twig', [ 'lang' => $_SESSION['lang'], 'trans' => $string, 'employees' => $array, 'success' => $success ] );
-			break;
+			echo $twig->render('employeesManagement/employeesManagement.twig', [ 'badge' => $badge, 'time' => $time, 'access_level' => $_SESSION['access_level'], 'lang' => $_SESSION['lang'], 'trans' => $string, 'employees' => $array, 'success' => $success ] );
+			break;		
 		case 'addEmployee':
-			echo $twig->render('employeesManagement/addEmployee.twig', [ 'lang' => $_SESSION['lang'], 'trans' => $string] );
-			break;
+			if($_SESSION['access_level'] >= 2){
+				echo $twig->render('employeesManagement/addEmployee.twig', [ 'badge' => $badge, 'time' => $time, 'access_level' => $_SESSION['access_level'], 'lang' => $_SESSION['lang'], 'trans' => $string] );
+				break;
+			}
+		case 'manageMenus':
+			if($_SESSION['access_level'] >= 2){
+				echo $twig->render('employeesManagement/employeesManagement.twig', [ 'badge' => $badge, 'time' => $time, 'access_level' => $_SESSION['access_level'], 'lang' => $_SESSION['lang'], 'trans' => $string] );
+				break;
+			}
 		default:
-			echo $twig->render('dashboard/dashboard.twig', [ 'trans' => $string ] );
+			echo $twig->render('dashboard/dashboard.twig', [ 'badge' => $badge, 'time' => $time,'id' => $_SESSION['id'], 'access_level' => $_SESSION['access_level'], 'trans' => $string ] );
 	}
 }else{
 	$string = LanguageController::translate('login');
