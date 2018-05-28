f581432a17f4e714da9348c032a07bb45f721495
WBPierre <d.pierrebox@gmail.com>
2018-05-28 19:59:13 +0200

    Updated front, translations button, order in process

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 
 require_once "config/config.php";
 $string = LanguageController::translate('index');
-$request = new DatabaseController();
-
 $url = LinkController::requestUrl($_GET['url']);
-
+$db = new DatabaseController();
+$response = $db->fetchAll('SELECT * FROM vnb_restaurant');
+foreach($response as $key=>$value){
+    $restaurant[$value['id']]=$value['name'];
+}
 
 if(isset($_SESSION['logUser']) && !empty($_SESSION['logUser'])){
-    $db = new DatabaseController();
+
     $request = $db->fetchAll('SELECT * FROM vnb_users WHERE id = :id',[ 'id' => $_SESSION['id']]);
     $name = $request[0]['firstname']." ".$request[0]['name'];
+
     switch($_GET['url']){
         case 'visit':
             header('Location: ThreeJS/index.html');
@@ -19,15 +22,15 @@ if(isset($_SESSION['logUser']) && !empty($_SESSION['logUser'])){
             $request = new ProductController();
             $array = $request->getAllProducts();
             $menu = $request->getAllMenus(true);
-            echo $twig->render('order/order.twig', [ 'trans' => $string, 'menus' => $menu, 'products' => $array ]);
+            echo $twig->render('order/order.twig', [ 'trans' => $string, 'menus' => $menu, 'products' => $array, 'lang' => $_SESSION['lang'] ]);
             break;
         case 'showMenu':
             $request = new ProductController();
             $array = $request->getAllProducts();
-            echo $twig->render('showMenu/showMenu.twig', ['products' => $array, 'trans' => $string, 'name' => $name ]);
+            echo $twig->render('showMenu/showMenu.twig', ['products' => $array, 'trans' => $string, 'name' => $name, 'lang' => $_SESSION['lang'] ]);
             break;
         default:
-            echo $twig->render('home/home.twig', [ 'trans' => $string, 'name'=>$name, 'intro' => true ] );
+            echo $twig->render('home/home.twig', [ 'trans' => $string, 'name'=>$name, 'intro' => true, 'restaurant' => $restaurant, 'lang' => $_SESSION['lang'] ] );
             break;
     }
 }else{
@@ -37,15 +40,15 @@ if(isset($_SESSION['logUser']) && !empty($_SESSION['logUser'])){
             header('Location: ThreeJS/index.html');
             break;
         case 'order':
-            echo $twig->render('order/order.twig', ['trans' => $string]);
+            echo $twig->render('order/order.twig', ['trans' => $string, 'lang' => $_SESSION['lang']]);
             break;
         case 'showMenu':
             $request = new ProductController();
             $array = $request->getAllProducts();
-            echo $twig->render('showMenu/showMenu.twig', ['products' => $array,'trans' => $string]);
+            echo $twig->render('showMenu/showMenu.twig', ['products' => $array,'trans' => $string, 'lang' => $_SESSION['lang']]);
             break;
         default:
-            echo $twig->render('home/home.twig', ['trans' => $string, 'intro' => true]);
+            echo $twig->render('home/home.twig', ['trans' => $string, 'intro' => true, 'restaurant' => $restaurant, 'lang' => $_SESSION['lang'] ]);
             break;
     }
 }
